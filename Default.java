063d842528e21dc40d7365edc619201778997c3f
Chandan Samal <chandansamal9800@gmail.com>
2022-04-02 22:21:46 +0530

    Added: 1.2 is done

---
+++ Default.java
@@ -0,0 +1,20 @@
+class Default{
+	static byte b;
+      static short s;
+      static char  c;
+      static int i;
+      static float f;
+      static long l;
+      static double d;
+	static boolean bb;
+	public static void main(String args[]){
+	System.out.println(b);
+	System.out.println(s);
+	System.out.println(c);
+	System.out.println(i);
+	System.out.println(f);
+	System.out.println(l);
+	System.out.println(d);
+	System.out.println(bb);
+	}
+}
