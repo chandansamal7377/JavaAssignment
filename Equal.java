40e19b6374bf0e66653a89f57d23bfd3dcdcee32
Chandan Samal <chandansamal9800@gmail.com>
2022-04-02 22:27:37 +0530

    Added: 1.3 is done

---
+++ Equal.java
@@ -0,0 +1,9 @@
+class Equal {
+	public static void main(String args[]) {
+	String s1=new String("Chandan");
+	String s2=new String("Chandan");
+	System.out.println(s1.equals(s2));
+	String s3=new String("Samal");
+	System.out.println(s1.equals(s3));
+	}
+}
