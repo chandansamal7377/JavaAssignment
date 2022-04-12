6c63f4bc3fc908ddfcb4ebe34a1cbba154ad76ab
Chandan Samal <chandansamal9800@gmail.com>
2022-04-12 15:07:21 +0530

    Added:  Checking Vowel or Consonant

---
+++ CheckVowelConsonant.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class CheckVowelConsonant {
+	public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the alphabet for checking vowel or consonant: ");
+	char  ch = sc.next().charAt(0);
+	switch(ch) {
+	case 'a':
+	   System.out.println(ch+ " is a vowel");
+	   break;
+	case 'e':
+	   System.out.println(ch+ "is a vowel");
+	   break;
+	case 'i':
+	   System.out.println(ch+ " is a vowel");
+	   break;
+	case 'o':
+	   System.out.println(ch +" is a vowel");
+	   break;
+	case 'u':
+	   System.out.println(ch +" is a vowel");
+	   break;
+	case 'A':
+	   System.out.println(ch+ " is a vowel");
+	   break;
+	case 'E':
+	   System.out.println(ch+ "is a vowel");
+	   break;
+	case 'I':
+	   System.out.println(ch+ " is a vowel");
+	   break;
+	case 'O':
+	   System.out.println(ch +" is a vowel");
+	   break;
+	case 'U':
+	   System.out.println(ch +" is a vowel");
+	   break;
+
+	default:
+	System.out.println(ch +" is a consonant");
+	break;
+      }
+    }
+}
